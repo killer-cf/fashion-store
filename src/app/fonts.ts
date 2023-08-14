3cec53f4c04c8fcc47ea0c8f09fa7b8541ea1baf
killer-cf <costa.kilder@live.com>
2023-08-14 15:16:40 -0300

    config fonts

---
+++ src/app/fonts.ts
@@ -0,0 +1,13 @@
+import { Tinos, Raleway } from 'next/font/google'
+
+export const tinos = Tinos({
+  weight: ['400', '700'],
+  subsets: ['cyrillic'],
+  variable: '--font-tinos',
+})
+
+export const raleway = Raleway({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-raleway',
+})
