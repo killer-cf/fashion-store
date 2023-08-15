fb2312f3ed3554fa13bf81ba24952691ac6ddf91
killer-cf <costa.kilder@live.com>
2023-08-15 10:49:59 -0300

    shopping cart icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { HandbagSimple } from '../libs/phosphor-react'
 import { Avatar } from './Avatar'
 import { SearchInput } from './SearchInput'
 
@@ -6,11 +7,15 @@ export function Header() {
     <header className="flex flex-col">
       <div className="flex justify-between bg-neutral-100 px-8 py-4">
         <SearchInput />
-        <div>
+        <div className="relative flex items-center gap-3">
           <Avatar
             size="sm"
             src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80"
           />
+          <HandbagSimple size={26} className="text-neutral-800" />
+          <div className="absolute right-[9px] top-[6px] text-xs font-bold text-neutral-800">
+            9
+          </div>
         </div>
       </div>
       <div className="bg-neutral-600 px-8 py-8 text-xl font-semibold text-neutral-300">
